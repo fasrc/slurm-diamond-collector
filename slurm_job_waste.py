edce03f075b440f7f19134337fbbd08f32d5806c
Paul Edmon <pedmon@cfa.harvard.edu>
2019-10-10 11:18:30 -0400

    Removing all tabs.

--- slurm_job_waste.py
+++ slurm_job_waste.py
@@ -26,9 +26,9 @@ class SlurmJobWasteCollector(diamond.collector.Collector):
         #Converts DD-HH:MM:SS to seconds
         if ('-' in t):
             (days, tempt) = t.split('-')
-	    else:
-	        tempt = t
-	        days = 0
+        else:
+            tempt = t
+            days = 0
 
         if (tempt.count(":") == 2):
             (hours, mins, secs) = tempt.split(':')
@@ -36,8 +36,8 @@ class SlurmJobWasteCollector(diamond.collector.Collector):
             (mins,secs) = tempt.split(':')
             hours = 0
         else:
-        	secs = tempt
-        	mins = 0
+            secs = tempt
+            mins = 0
             hours = 0
         tsec = 86400.0*float(days)+3600.0*float(hours)+60.0*float(mins)+float(secs)
         return tsec
@@ -59,7 +59,7 @@ class SlurmJobWasteCollector(diamond.collector.Collector):
         try:
             # Grab data from slurm
             proc = subprocess.Popen(
-	                'sacct -S {hour}:00 -E now --state=CD --units=G -n -P -o user,Account,ReqCPUS,NNodes,ReqMEM,MaxRSS,Elapsed,TotalCPU'.format(
+                    'sacct -S {hour}:00 -E now --state=CD --units=G -n -P -o user,Account,ReqCPUS,NNodes,ReqMEM,MaxRSS,Elapsed,TotalCPU'.format(
                         hour=datetime.now().hour
                     ).split(),
                     stdout=subprocess.PIPE
@@ -70,50 +70,50 @@ class SlurmJobWasteCollector(diamond.collector.Collector):
         else:
             for line in proc.stdout:
                 # Split out data
-	            (LUser, LAccount, LReqCPUS, LNNodes, LReqMem, LMaxRSS, LElapsed, LTotalCPU) = line.strip().split('|')
-	            if LUser != "":
+                (LUser, LAccount, LReqCPUS, LNNodes, LReqMem, LMaxRSS, LElapsed, LTotalCPU) = line.strip().split('|')
+                if LUser != "":
                     # If on initial job entry, pull majority of data
-		            if User != "":
+                    if User != "":
                         # If this the start of a new Job permanently store data
-			            cpustats[User] = float(cpustats[User])+CPUWastedTRES
-			            memstats[User] = float(memstats[User])+MemWastedTRES
+                        cpustats[User] = float(cpustats[User])+CPUWastedTRES
+                        memstats[User] = float(memstats[User])+MemWastedTRES
 
                 # Data
-		        User = LUser
-		        if User in memstats:
-	                continue
-		        else:
-		            memstats[User]=0
+                User = LUser
+                if User in memstats:
+                    continue
+                else:
+                    memstats[User]=0
 
-        		if User in cpustats:
-        	        continue
-		        else:
-		            cpustats[User]=0
+                if User in cpustats:
+                    continue
+                else:
+                    cpustats[User]=0
 
-		        account[User]=LAccount
-		        ReqCPUS = LReqCPUS
-		        NNodes = LNNodes
-		        ReqMem = LReqMem
-		        Elapsed = LElapsed
-		        TotalCPU = LTotalCPU
-		        MaxRSS = 0
+                account[User]=LAccount
+                ReqCPUS = LReqCPUS
+                NNodes = LNNodes
+                ReqMem = LReqMem
+                Elapsed = LElapsed
+                TotalCPU = LTotalCPU
+                MaxRSS = 0
 
                 if ('Gn' in ReqMem):
                     ReqMem=ReqMem.strip("Gn")
                 if ('Gc' in ReqMem):
                     ReqMem=float(ReqMem.strip("Gc"))*float(ReqCPUS)/float(NNodes)
 
-		        # Now to compute CPU Wasted Tres
+                # Now to compute CPU Wasted Tres
                 elapsedt = self.convert2sec(Elapsed)
 
-		        totalcput = self.convert2sec(TotalCPU))
+                totalcput = self.convert2sec(TotalCPU))
 
-		        CPUWastedTRES=max(0,CPUTRESWeight*(float(ReqCPUS)*float(elapsedt)-float(totalcput)))
-	        else:
-		        MaxRSS=max(MaxRSS,LMaxRSS.strip("G"))
+                CPUWastedTRES=max(0,CPUTRESWeight*(float(ReqCPUS)*float(elapsedt)-float(totalcput)))
+            else:
+                MaxRSS=max(MaxRSS,LMaxRSS.strip("G"))
 
-		        # Now to compute Mem Wasted Tres
-		        MemWastedTRES=max(0,MemTRESWeight*(float(ReqMem)-float(MaxRSS))*float(elapsedt))		
+                # Now to compute Mem Wasted Tres
+                MemWastedTRES=max(0,MemTRESWeight*(float(ReqMem)-float(MaxRSS))*float(elapsedt))        
 
             cpustats[User] = float(cpustats[User])+CPUWastedTRES
             memstats[User] = float(memstats[User])+MemWastedTRES
